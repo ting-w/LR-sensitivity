f56058315b8c8a4138fe41022397ab18a16f6251
ting-w <36026005+ting-w@users.noreply.github.com>
2018-05-02 15:04:12 -0400

    Update model.m

--- model.m
+++ model.m
@@ -1,5 +1,4 @@
 function mymodel = model
-%A<==>B<==>C
 mymodel.nu = [1    -1     0     0     0     0     0     0     0     0     0     0;
               0     0     1    -1    -2     2     0     0     0     0    -1     1;
               0     0     0     0     1    -1     0     0     0     0     0     0;
